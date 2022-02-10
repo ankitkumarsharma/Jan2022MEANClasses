5d3e41686bfd9f6657e55db615dd386027880a01
ankitkumarsharma <aksharma.dixit@gmail.com>
2022-02-10 09:50:10 +0530

    email sent added

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 var http = require('http');
 var url = require('url');
-
+var nodeEmail = require('nodemailer');
+// server create
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type':'text/html'});
     var query1 = url.parse(req.url, true).query;
     var content = query1.abc + " "+query1.xyz;
     res.end(content);
 }).listen(8080);
+console.log('Server running')
+
+var emailManage = nodeEmail.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'abc@gmail.com',
+        pass:'12345'
+    }
+});
+
+var emailOptions = {
+    from: 'abc@gmail.com',
+    to: 'aks.computerexpert@gmail.com',
+    subject:'ABC XYZ',
+    text: 'content'
+};
+
+emailManage.sendMail(emailOptions, (err, info)=>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log("Email sent", info.response);
+    }
+})
